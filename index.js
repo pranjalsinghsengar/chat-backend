3607644252f3c569054c236cc34f8d789b607aff
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2024-03-31 03:26:01 +0530

    -major updates

--- index.js
+++ index.js
@@ -1,104 +1,41 @@
-require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
 const app = express();
-const port = 8080;
-const cors = require("cors");
-app.use(cors());
+app.use(express.json());
+const mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({ extends: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-const jwt = require("jsonwebtoken");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 mongoose
   .connect(
-    "mongodb+srv://sengar:sengar123@cluster0.h8dnflv.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
-    // {useNewUrIParser: true, useUnifiedTop010gy: true, useCreateIndex: true},
+    "mongodb+srv://cluster0.h8dnflv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      dbName: "base0",
+      user: "sengar",
+      pass: "sengar123",
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    }
   )
   .then(() => {
-    console.log("connected to Mongoose");
+    console.log("Mongo Connected");
   })
   .catch((err) => {
-    console.log("err in mongodb", err);
+    console.error(err);
   });
 
-app.listen(port, () => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log("server listening on port", port);
+//   mongodb+srv://sengar:<password>@cluster0.h8dnflv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const UserRoute = require("./routes/User_route");
+app.use("/", UserRoute);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
   });
 });
-
-const User = require("./models/users");
-// const Message = require('./models/message');
-
-// end points for ragistration of the user
-
-const createToken = (userId) => {
-  const payload = {
-    userId: userId,
-  };
-  const token = jwt.sign(payload, "Q$r2K6W8n!jCW%Zk", { expireIn: "1h" });
-  return token;
-};
-
-app.post("/register", async (req, res) => {
-  const { name, email, password, image } = req.body;
-
-  //   create new user
-  const newUser = new User({
-    name,
-    email,
-    password,
-    image,
-  });
-
-  // save the user to the database
-
-  await newUser
-    .save()
-    .then(() => {
-      res.status(200).json({
-        massage: "user registered ssdfuccessfully",
-        user: { name, email, password, image },
-      });
-    })
-    .catch((err) => {
-      console.log("err in ragister user", err);
-      res.status(500).json({ massage: "error in ragister user" });
-    });
-});
-
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(404).json({ message: "email and password are required" });
-  }
-
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        // if user not found
-        return res.status(404).json({ message: "user not found" });
-      }
-
-      // compare
-      if (user.password !== password) {
-        return res.status(404).json({ message: "invalid password" });
-      }
-      res.status(200).json({message: 'user found', user});
-      // const token = createToken(user._id);
-      // res.status(200).json({token: token});
-    })
-    .catch((err) => {
-      console.log("err in finding user", err);
-      res.status(500).json({ message: "internal server error!" });
-    });
-});
-
-app.get("/data", (req, res) => {});
---
+++ routes/User_route.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const createErrors = require("http-errors");
+const User = require("../models/users");
+const mongoose = require("mongoose");
+
+router.get("/:email", async (req, res, next) => {
+  const email = req.params.email;
+  try {
+    const user = await User.findOne({ email: email }, {});
+
+    if (!user) {
+      throw createErrors("404", "user not exist");
+    }
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    if (error instanceof mongoose.CastError) {
+      next(createErrors("404", "Invalid email"));
+    }
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const user = await new User(req.body);
+    const result = await user.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // console.log(req.body);
+  // const product = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  //   image: req.body.image,
+  // });
+  // product
+  //   .save()
+  //   .then((result) => {
+  //     console.log(result);
+  //     res.send(result);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
+});
+
+module.exports = router;
